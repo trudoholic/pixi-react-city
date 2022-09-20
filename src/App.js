efc54b4981078c72038457f16a65eafdbc6c50e2
Oleg <35769958+trudoholic@users.noreply.github.com>
2022-09-20 17:17:10 +0300

    text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { Stage, Sprite } from '@inlet/react-pixi'
+import { Stage, Container, Sprite, Text } from '@inlet/react-pixi'
 
 function App() {
   return (
-      <Stage>
-        <Sprite image="./logo192.png" x={100} y={100} />
+      // <Stage width={300} height={300} options={{ backgroundAlpha: 0 }}>
+      <Stage width={300} height={300} options={{ backgroundColor: 0x336699, antialias: true }}>
+        {/*<Sprite image="./logo192.png" x={100} y={100} />*/}
+
+        <Container x={150} y={150}>
+          {/*<Text text="Hello World" filter={[blurFilter]} />*/}
+          <Text text="Hello World" />
+        </Container>
       </Stage>
   );
 }
