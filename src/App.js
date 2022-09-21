fead56ba1f1d0dd94eaf24d488a2f11804188f0d
Oleg <35769958+trudoholic@users.noreply.github.com>
2022-09-21 10:09:45 +0300

    sprite

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { Stage, Container, Sprite, Text } from '@inlet/react-pixi'
 function App() {
   return (
       // <Stage width={300} height={300} options={{ backgroundAlpha: 0 }}>
-      <Stage width={300} height={300} options={{ backgroundColor: 0x336699, antialias: true }}>
-        {/*<Sprite image="./logo192.png" x={100} y={100} />*/}
+      <Stage width={800} height={600} options={{ backgroundColor: 0x336699, antialias: true }}>
+        <Sprite image="./assets/green.png" x={100} y={100} />
 
         <Container x={150} y={150}>
           {/*<Text text="Hello World" filter={[blurFilter]} />*/}
@@ -17,4 +17,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
