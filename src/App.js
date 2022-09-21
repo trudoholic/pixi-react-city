651c666ba0cdd337301755262bbefa7eece81243
Oleg <35769958+trudoholic@users.noreply.github.com>
2022-09-21 10:21:25 +0300

    TextStyle

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
+import * as PIXI from 'pixi.js'
 import { Stage, Container, Sprite, Text } from '@inlet/react-pixi'
+
+const textStyle = new PIXI.TextStyle({
+    align: "center",
+    fontWeight: "bold",
+    fill: "#fff",
+    // fill: ["#26f7a3", "#01d27e"],
+    // stroke: "#eef1f5",
+    // strokeThickness: 1,
+    // letterSpacing: 5,
+    // wordWrap: false,
+    // wordWrapWidth: 350
+})
 
 function App() {
   return (
-      // <Stage width={300} height={300} options={{ backgroundAlpha: 0 }}>
       <Stage width={800} height={600} options={{ backgroundColor: 0x336699, antialias: true }}>
         <Sprite image="./assets/green.png" x={100} y={100} />
 
-        <Container x={150} y={150}>
+        <Container x={8} y={8}>
           {/*<Text text="Hello World" filter={[blurFilter]} />*/}
-          <Text text="Hello World" />
+          <Text text="Hello World" style={textStyle} />
         </Container>
       </Stage>
   );
 }
 
 export default App
+
+//{ backgroundAlpha: 0 }
