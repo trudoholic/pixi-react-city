998397879493c08dd5187e64f3d48d8c3b8f76c6
Oleg <35769958+trudoholic@users.noreply.github.com>
2022-11-06 15:07:30 +0300

    setRooms

--- src/components/RoomLayer.jsx
+++ src/components/RoomLayer.jsx
@@ -1,35 +1,54 @@
+import {useState} from "react"
 import * as PIXI from "pixi.js"
 // import {Container, Sprite} from "@inlet/react-pixi/animated"
 import {Container, Sprite} from '@inlet/react-pixi'
 
-const ROOM_W = 32, ROOM_H = 24, GAP = 1
-const ROWS = 3, COLS = 4, ROOMS = []
-// const ROOMS = [...Array(4 * 3)].map((it, i) => ({}))
+const ROOM_W = 320, ROOM_H = 24, GAP = 1
 
-for (let row = 0; row < ROWS; ++row) {
-    for (let col = 0; col < COLS; ++col) {
-        ROOMS.push({
-            name: row + '_' + col,
-            x: ROOM_W/2 + col * (ROOM_W + GAP),
-            y: ROOM_H/2 + row * (ROOM_H + GAP),
-        })
+const prepareRooms = (rows, cols) => {
+    console.log('prepareRooms:', rows, cols)
+    const rooms = []
+    for (let row = 0; row < rows; ++row) {
+        for (let col = 0; col < cols; ++col) {
+            rooms.push({
+                name: 'room_' + rooms.length,
+                selected: false,
+                x: ROOM_W/2 + col * (ROOM_W + GAP),
+                y: ROOM_H/2 + row * (ROOM_H + GAP),
+            })
+        }
     }
+    return rooms
 }
 
 const RoomLayer = props => {
 
+    const [rooms, setRooms] = useState(() => prepareRooms(3, 4))
+    const [selName, setSelName] = useState('')
+
     return (
         <Container name={'rooms'}
-            x={64} y={64} interactive
+            x={0} y={0} interactive
             click={e => {
                 console.log('RoomLayer:', e.target.name, e.currentTarget.name)
+                // if (selName === e.target.name) return
+
+                const nextName = selName === e.target.name ? '' : e.target.name
+                const nextRooms = rooms.map(room => (
+                    room.name === selName ? {...room, selected: false} :
+                    room.name === nextName ? {...room, selected: true} :
+                    room
+                ))
+                setRooms(nextRooms)
+                setSelName(nextName)
             }}
         >
-            {ROOMS.map((it, i) => (
+            {rooms.map(it => (
                 <Sprite key={it.name} name={it.name}
-                    anchor={0.5} x={it.x} y={it.y} width={ROOM_W} height={ROOM_H}
-                    alpha={.5} tint={0xff5722}
-                    texture={PIXI.Texture.WHITE} // PIXI.Texture.WHITE size is 10x10
+                    anchor={0.5} x={it.x} y={it.y}
+                    alpha={it.selected? 1:.5} tint={0xff5722}
+                    texture={PIXI.Texture.WHITE}
+                    width={ROOM_W} height={ROOM_H}
                     interactive buttonMode
                 />
             ))}
