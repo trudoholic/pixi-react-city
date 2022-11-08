db425bf7f441fa02339a4c802e78901d5b35c325
Oleg <35769958+trudoholic@users.noreply.github.com>
2022-11-08 15:34:27 +0300

    GameState rf

--- src/components/GameState.js
+++ src/components/GameState.js
@@ -5,25 +5,46 @@ export function useStateContext() {
     return useContext(StateContext)
 }
 
-const StateDispatchContext = createContext((action) => {})
+const StateDispatchContext = createContext(action => {})
 export function useStateDispatchContext() {
     return useContext(StateDispatchContext)
 }
 
-const initialState = []
-const init = v => v
+const ROOM_W = 320, ROOM_H = 240, GAP = 1
+const prepareRooms = (rows, cols) => {
+    console.log('prepareRooms:', rows, cols)
+    const rooms = []
+    for (let row = 0; row < rows; ++row) {
+        for (let col = 0; col < cols; ++col) {
+            rooms.push({
+                name: 'room_' + rooms.length,
+                selected: false,
+                x: col * (ROOM_W + GAP),
+                y: row * (ROOM_H + GAP),
+                width: ROOM_W, height: ROOM_H
+            })
+        }
+    }
+    return rooms
+}
+
+const initialState = prepareRooms(3, 4)
 
 const reducer = (state, action) => {
     switch (action.type) {
         case 'ADD':
             return [...state, action.payload]
+        case 'SELECT':
+            return state.map(room => (
+                room.name === action.payload ? {...room, selected: !room.selected} : {...room, selected: false}
+            ))
         default:
             return state
     }
 }
 
 export const StateContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState, init)
+    const [state, dispatch] = useReducer(reducer, initialState, v => v)
 
     return (
         <StateContext.Provider value={state}>
--- src/components/RoomLayer.jsx
+++ src/components/RoomLayer.jsx
@@ -1,4 +1,3 @@
-import {useState} from "react"
 import * as PIXI from "pixi.js"
 // import {Container, Sprite} from "@inlet/react-pixi/animated"
 import {Container, Sprite} from '@inlet/react-pixi'
@@ -6,72 +5,29 @@ import {Container, Sprite} from '@inlet/react-pixi'
 import { useStateContext, useStateDispatchContext } from "./GameState"
 import StyledText from "./StyledText";
 
-const ROOM_W = 320, ROOM_H = 240, GAP = 1
-
-const prepareRooms = (rows, cols) => {
-    console.log('prepareRooms:', rows, cols)
-    const rooms = []
-    for (let row = 0; row < rows; ++row) {
-        for (let col = 0; col < cols; ++col) {
-            rooms.push({
-                name: 'room_' + rooms.length,
-                selected: false,
-                // x: ROOM_W/2 + col * (ROOM_W + GAP),
-                // y: ROOM_H/2 + row * (ROOM_H + GAP),
-                x: col * (ROOM_W + GAP),
-                y: row * (ROOM_H + GAP),
-            })
-        }
-    }
-    return rooms
-}
-
 const RoomLayer = props => {
 
     const state = useStateContext()
     const dispatch = useStateDispatchContext()
-
-    const [rooms, setRooms] = useState(() => prepareRooms(3, 4))
-    const [selName, setSelName] = useState('')
 
     return (
         <Container name={'rooms'}
             x={0} y={0} interactive
             click={e => {
                 console.log('RoomLayer:', e.target.name, e.currentTarget.name)
-
-                console.log('state:', state.length)
                 dispatch({
-                    type: 'ADD',
-                    payload: 42
+                    type: 'SELECT',
+                    payload: e.target.name
                 })
-
-                // if (selName === e.target.name) return
-
-                const nextName = selName === e.target.name ? '' : e.target.name
-                const nextRooms = rooms.map(room => (
-                    room.name === selName ? {...room, selected: false} :
-                    room.name === nextName ? {...room, selected: true} :
-                    room
-                ))
-                setRooms(nextRooms)
-                setSelName(nextName)
             }}
         >
-            {rooms.map(it => (
-                // <Sprite key={it.name} name={it.name}
-                //     anchor={0.5} x={it.x} y={it.y}
-                //     alpha={it.selected? 1:.5} tint={0xff5722}
-                //     texture={PIXI.Texture.WHITE}
-                //     width={ROOM_W} height={ROOM_H}
-                //     interactive buttonMode
-                // />
+            {state.map(it => (
                 <Container key={it.name} x={it.x} y={it.y}>
                     <Sprite name={it.name}
-                        //anchor={0.5} //x={it.x} y={it.y}
-                        alpha={it.selected? 1:.5} tint={0xff5722}
+                        alpha={it.selected? 1:.5}
                         texture={PIXI.Texture.WHITE}
-                        width={ROOM_W} height={ROOM_H}
+                        tint={0xff5722}
+                        width={it.width} height={it.height}
                         interactive buttonMode
                     />
                     <StyledText text={it.name} x={8} y={8} />
